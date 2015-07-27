81ec9c84759a04297674c6a3f857d94b089d7cfc
Mathias Fredriksson <mafredri@gmail.com>
2015-07-27 17:16:48 +0300

    Store output in string and echo once

--- pretty-time.zsh
+++ pretty-time.zsh
@@ -5,14 +5,15 @@ if [ $# -eq 0 ]; then
 	exit 1
 fi
 
-local tmp=$1
+local human tmp=$1
 local days=$(( tmp / 60 / 60 / 24 ))
 local hours=$(( tmp / 60 / 60 % 24 ))
 local minutes=$(( tmp / 60 % 60 ))
 local seconds=$(( tmp % 60 ))
 
-(( $days > 0 )) && echo -n "${days}d "
-(( $hours > 0 )) && echo -n "${hours}h "
-(( $minutes > 0 )) && echo -n "${minutes}m "
+(( $days > 0 )) && human+="${days}d "
+(( $hours > 0 )) && human+="${hours}h "
+(( $minutes > 0 )) && human+="${minutes}m "
+human+="${seconds}s"
 
-echo "${seconds}s"
+echo "$human"
